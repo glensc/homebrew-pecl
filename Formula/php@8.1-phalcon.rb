1e79fffe13bb46c496224b544c64e7610e2d6817
Elan Ruusamäe <glen@delfi.ee>
2026-05-08 00:23:49 +0300

    Update Phalcon to 5.11.1

--- Formula/php@8.1-phalcon.rb
+++ Formula/php@8.1-phalcon.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT81Phalcon < PhpPeclFormula
   extension_dsl "Phalcon is a full stack PHP framework offering low resource consumption and high performance"
 
-  url "https://pecl.php.net/get/phalcon-5.9.3.tgz"
-  sha256 "2b1983f09b56fc2779509a8ac1df776c368782538a7ef6601c0d4aef9892fe83"
+  url "https://pecl.php.net/get/phalcon-5.11.1.tgz"
+  sha256 "8331f47cf760dbaf13ae2d77d63971005b03df40279dec97ede4b537d14c9591"
   license "BSD-3-Clause"
 
   bottle do
